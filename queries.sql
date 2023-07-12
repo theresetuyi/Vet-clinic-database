f9a42dc91c6a7729961b50ac92583272a39ca880
Therese Tuyisabe <teddytiyi@gmail.com>
2023-07-12 12:42:57 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,9 +2,9 @@
 
 SELECT * from animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name FROM animals WHERE neutered = true AND escape_attempt < 3;
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attempt FROM animals WHERE weight_kg > 10.5;
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE name != 'Gabumon';
